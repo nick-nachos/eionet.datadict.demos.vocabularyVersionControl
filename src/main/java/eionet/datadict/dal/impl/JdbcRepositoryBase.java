180db96ffa188a793a6dd651205d0fa73a58e0b5
Nikos Nakas <nick.nachos86@gmail.com>
2016-05-10 11:59:20 +0300

    Removed unused imports

--- src/main/java/eionet/datadict/dal/impl/JdbcRepositoryBase.java
+++ src/main/java/eionet/datadict/dal/impl/JdbcRepositoryBase.java
@@ -1,12 +1,8 @@
 package eionet.datadict.dal.impl;
 
-import eionet.datadict.dal.impl.util.ResultSetUtils;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import javax.sql.DataSource;
-import org.joda.time.DateTime;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
 
 public abstract class JdbcRepositoryBase extends NamedParameterJdbcDaoSupport {
