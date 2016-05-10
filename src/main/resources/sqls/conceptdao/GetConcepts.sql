018ea56f26bc8b1a9a111b8e4b3347c2364dcdca
Nikos Nakas <nick.nachos86@gmail.com>
2016-05-10 13:52:07 +0300

    Removed useless inner join

--- src/main/resources/sqls/conceptdao/GetConcepts.sql
+++ src/main/resources/sqls/conceptdao/GetConcepts.sql
@@ -2,14 +2,10 @@
 select
     c.*
 from
-    Vocabulary v
-inner join
     VocabularyConcepts vc
-on 
-    v.Id = vc.fVocabularyId
 inner join
     Concept c
 on
     vc.fConceptId = c.Id
 where
-    v.Id = :vocabularyId
+    vc.fVocabularyId = :vocabularyId
