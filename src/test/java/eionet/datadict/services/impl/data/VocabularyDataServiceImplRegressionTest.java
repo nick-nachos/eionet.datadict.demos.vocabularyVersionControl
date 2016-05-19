9681a573119b2b07ec5c6ab52b34f9919b61fa9f
Nikos Nakas <nick.nachos86@gmail.com>
2016-05-19 10:55:20 +0300

    Added testing of entity inter-links in vocabulary regression tests

--- src/test/java/eionet/datadict/services/impl/data/VocabularyDataServiceImplRegressionTest.java
+++ src/test/java/eionet/datadict/services/impl/data/VocabularyDataServiceImplRegressionTest.java
@@ -6,6 +6,9 @@ import eionet.datadict.dal.ConceptDao;
 import eionet.datadict.dal.VocabularyDao;
 import eionet.datadict.dal.versioning.RevisionDao;
 import eionet.datadict.model.Vocabulary;
+import eionet.datadict.model.VocabularyConcept;
+import eionet.datadict.model.VocabularyConceptAttributeValue;
+import eionet.datadict.model.VocabularyConceptAttributeValueSet;
 import eionet.datadict.model.testutil.VocabularyTestUtils;
 import eionet.datadict.services.data.VocabularyDataService;
 import eionet.datadict.testutil.ExecutionDurationHandler;
@@ -16,9 +19,11 @@ import eionet.datadict.testutil.dal.ConceptDaoTestDecorator;
 import eionet.datadict.testutil.dal.VocabularyDaoTestDecorator;
 import eionet.datadict.testutil.dal.versioning.RevisionDaoTestDecorator;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsIn.isIn;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.hamcrest.core.IsNull.nullValue;
+import static org.hamcrest.core.IsSame.sameInstance;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -88,6 +93,7 @@ public class VocabularyDataServiceImplRegressionTest {
         assertThat("Concept count missmatch", v.getConcepts().size(), is(39));
         assertThat("Concept attribute value sets count missmatch", VocabularyTestUtils.countConceptAttributeValueSets(v), is(28));
         assertThat("Concept attribute values count missmatch", VocabularyTestUtils.countConceptAttributeValues(v), is(28));
+        this.assertEntityLinks(v);
     }
     
     @Test
@@ -98,6 +104,7 @@ public class VocabularyDataServiceImplRegressionTest {
         assertThat("Concept count missmatch", v.getConcepts().size(), is(614));
         assertThat("Concept attribute value sets count missmatch", VocabularyTestUtils.countConceptAttributeValueSets(v), is(1228));
         assertThat("Concept attribute values count missmatch", VocabularyTestUtils.countConceptAttributeValues(v), is(1228));
+        this.assertEntityLinks(v);
     }
     
     @Test
@@ -108,6 +115,7 @@ public class VocabularyDataServiceImplRegressionTest {
         assertThat("Concept count missmatch", v.getConcepts().size(), is(2940));
         assertThat("Concept attribute value sets count missmatch", VocabularyTestUtils.countConceptAttributeValueSets(v), is(3380));
         assertThat("Concept attribute values count missmatch", VocabularyTestUtils.countConceptAttributeValues(v), is(3406));
+        this.assertEntityLinks(v);
     }
     
     @Test
@@ -118,6 +126,7 @@ public class VocabularyDataServiceImplRegressionTest {
         assertThat("Concept count missmatch", v.getConcepts().size(), is(36682));
         assertThat("Concept attribute value sets count missmatch", VocabularyTestUtils.countConceptAttributeValueSets(v), is(36682));
         assertThat("Concept attribute values count missmatch", VocabularyTestUtils.countConceptAttributeValues(v), is(36682));
+        this.assertEntityLinks(v);
     }
     
     @Test
@@ -128,6 +137,43 @@ public class VocabularyDataServiceImplRegressionTest {
         assertThat("Concept count missmatch", v.getConcepts().size(), is(196696));
         assertThat("Concept attribute value sets count missmatch", VocabularyTestUtils.countConceptAttributeValueSets(v), is(677024));
         assertThat("Concept attribute values count missmatch", VocabularyTestUtils.countConceptAttributeValues(v), is(677024));
+        this.assertEntityLinks(v);
+    }
+    
+    private void assertEntityLinks(Vocabulary vocabulary) {
+        for (VocabularyConcept concept : vocabulary.getConcepts()) {
+            assertThat(concept.getVocabulary(), is(notNullValue()));
+            assertThat(concept.getVocabulary(), is(sameInstance(vocabulary)));
+            
+            for (VocabularyConceptAttributeValueSet valueSet : concept.getAttributeValues()) {
+                assertThat(valueSet.getConcept(), is(notNullValue()));
+                assertThat(valueSet.getConcept(), is(sameInstance(concept)));
+                
+                assertThat(valueSet.getAttribute(), is(notNullValue()));
+                assertThat(valueSet.getAttribute(), isIn(vocabulary.getConceptAttributes()));
+                
+                for (VocabularyConceptAttributeValue value : valueSet.getValues()) {
+                    assertThat(value.getValueSet(), is(notNullValue()));
+                    assertThat(value.getValueSet(), is(sameInstance(valueSet)));
+                    
+                    if (value.getValue() == null) {
+                        VocabularyConcept relatedConcept = value.getRelatedConcept();
+                        assertThat(relatedConcept, is(notNullValue()));
+                        assertThat(relatedConcept.getId(), is(notNullValue()));
+                        assertThat(relatedConcept.getIdentifier(), is(notNullValue()));
+                        
+                        Vocabulary relatedVocabulary = relatedConcept.getVocabulary();
+                        assertThat(relatedVocabulary, is(notNullValue()));
+                        assertThat(relatedVocabulary.getId(), is(notNullValue()));
+                        assertThat(relatedVocabulary.getIdentifier(), is(notNullValue()));
+                        
+                    }
+                    else {
+                        assertThat(value.getRelatedConcept(), is(nullValue()));
+                    }
+                }
+            }
+        }
     }
     
     private final class GetLatestVocabularyExecutionDurationPrinter extends ExecutionDurationPrinter<Vocabulary> {
