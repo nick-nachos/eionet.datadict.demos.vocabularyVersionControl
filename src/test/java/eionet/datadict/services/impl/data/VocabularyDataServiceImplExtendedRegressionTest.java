2c4227e63602c706b25714f9c6a7e5ec78ef9e30
Nikos Nakas <nick.nachos86@gmail.com>
2016-05-24 12:53:55 +0300

    Maven parameterized seed data expansion

--- src/test/java/eionet/datadict/services/impl/data/VocabularyDataServiceImplExtendedRegressionTest.java
+++ src/test/java/eionet/datadict/services/impl/data/VocabularyDataServiceImplExtendedRegressionTest.java
@@ -1,44 +1,44 @@
 package eionet.datadict.services.impl.data;
 
 import eionet.datadict.testutil.ExecutionDurationPrinter;
+import eionet.datadict.testutil.services.data.SeedDataExpanderConfiguration;
 import eionet.datadict.testutil.services.data.SeedDataExpanderService;
-import org.junit.AfterClass;
+import org.junit.Assume;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@Ignore
 @ContextConfiguration(locations = { "classpath:spring-config.xml", "classpath:spring-test-config.xml" })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class VocabularyDataServiceImplExtendedRegressionTest extends VocabularyDataServiceImplRegressionTest {
     
     private static SeedDataExpanderService seedDataExpanderService;
+    private static SeedDataExpanderConfiguration seedDataExpanderConfiguration;
     
     private static boolean dbSetupDone;
     
     @Autowired
     private SeedDataExpanderService seedDataExpanderServiceInstance;
+    @Autowired
+    private SeedDataExpanderConfiguration seedDataExpanderConfigurationInstance;
     
     @Before
     @Override
     public void setUp() {
         super.setUp();
         
+        Assume.assumeTrue(this.seedDataExpanderConfigurationInstance.getExpansionFactor() > 0);
+        
         if (dbSetupDone) {
             return;
         }
         
         seedDataExpanderService = seedDataExpanderServiceInstance;
+        seedDataExpanderConfiguration = seedDataExpanderConfigurationInstance;
         initDbData();
         dbSetupDone = true;
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-        restoreDbData();
     }
     
     private static void initDbData() {
@@ -51,12 +51,12 @@ public class VocabularyDataServiceImplExtendedRegressionTest extends VocabularyD
 
             @Override
             protected Object execute() throws Exception {
-                int genSize = 50;
+                int genSize = seedDataExpanderConfiguration.getExpansionFactor();
                 
                 for (int i = 0; i < genSize; i++) {
+                    System.out.format("Expanding %d out of %d... ", i + 1, genSize);
                     seedDataExpanderService.expandSeedData();
-                    
-                    System.out.format("Gen %d out of %d complete.%n", i + 1, genSize);
+                    System.out.format("Done.%n", i + 1, genSize);
                 }
                 
                 return null;
@@ -64,10 +64,6 @@ public class VocabularyDataServiceImplExtendedRegressionTest extends VocabularyD
         };
         
         printer.run();
-    }
-    
-    private static void restoreDbData() {
-        
     }
     
 }
---
+++ src/test/java/eionet/datadict/testutil/services/data/SeedDataExpanderConfiguration.java
@@ -0,0 +1,15 @@
+package eionet.datadict.testutil.services.data;
+
+public class SeedDataExpanderConfiguration {
+
+    private int expansionFactor;
+
+    public int getExpansionFactor() {
+        return expansionFactor;
+    }
+
+    public void setExpansionFactor(int expansionFactor) {
+        this.expansionFactor = expansionFactor;
+    }
+    
+}
